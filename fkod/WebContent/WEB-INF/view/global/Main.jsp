9c10aea2f42f54764070e3d0d3f082e8d62dc974
HB <HB@HB-PC>
2015-11-30 18:06:52 +0900

    2015.11.30 오후6시

--- fkod/WebContent/WEB-INF/view/global/Main.jsp
+++ fkod/WebContent/WEB-INF/view/global/Main.jsp
@@ -1,12 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <h1>하하</h1>
-<!-- <script type="text/javascript">
-	아래 코드를 주석해제하고
-	각자 주소를 바꿔서 자기 페이지를 메인으로 
-	프로그래밍 시작하시기 바랍니다.
-	ex) 성한이
+<script type="text/javascript">
 	$(function() {
 		console.log("메인시작");
 		location.href = "${context}/member/Member.do";
 	});
-</script> -->
+</script>
--- fkod/src/member/MemberController.java
+++ fkod/src/member/MemberController.java
@@ -7,16 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import global.DispatcherServlet;
+import global.Seperator;
+
 /**
  * Servlet implementation class MemberController
  */
-@WebServlet("/MemberController")
+@WebServlet("/member/Member.do")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void service(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-	
+		Seperator.init(request);
+		DispatcherServlet.getInstance().runDispatcher(request, response, Seperator.command.getView());
 	}
 
 }
