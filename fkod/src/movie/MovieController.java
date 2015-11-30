5796f14a11fd23f28896077810507a76a43a4ea4
user <user@user-PC>
2015-11-30 17:54:57 +0900

    11.30 끝

--- fkod/src/movie/MovieController.java
+++ fkod/src/movie/MovieController.java
@@ -1,8 +1,37 @@
 package movie;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class MovieController extends HttpServlet{
-	
-	
+import global.DispatcherServlet;
+import global.Seperator;
+
+/**
+ * Servlet implementation class MovieController
+ */
+@WebServlet("/movie/Movie.do")
+public class MovieController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Seperator.init(request);
+		DispatcherServlet.getInstance().runDispatcher(request, response, Seperator.command.getView());
+	}
+
 }
